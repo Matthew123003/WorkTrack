93fad485f3bf39c316ba481e259eefb77ca0db82
matthew123003 <matthew123003@gmail.com>
2024-08-31 15:28:18 -0400

    Writing internship tests

--- TekTrackBackEnd/Track/src/test/java/com/Tek/Track/InternshipTests/InternshipServiceTests.java
+++ TekTrackBackEnd/Track/src/test/java/com/Tek/Track/InternshipTests/InternshipServiceTests.java
@@ -1,4 +1,18 @@
 package com.Tek.Track.InternshipTests;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.time.LocalDate;
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.Tek.Track.Models.Internship;
+import com.Tek.Track.Repositories.InternshipRepository;
+import com.Tek.Track.Services.InternshipService;
+
 public class InternshipServiceTests {
 }
