108791053c219a54e50a727236b0e42970b96cf4
matthew123003 <matthew123003@gmail.com>
2024-08-31 15:23:30 -0400

    OAUTH2 JWT AUTHENTICATION COMPLETE AND RUNNING

--- TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Models/User.java
+++ TekTrackBackEnd/Track/src/main/java/com/Tek/Track/Models/User.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
 @Entity
 @Table(name = "User")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
