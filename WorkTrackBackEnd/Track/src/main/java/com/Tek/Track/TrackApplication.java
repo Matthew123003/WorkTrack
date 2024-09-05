847ccdc80cc936327da5b49bcbf074ff818edfeb
matthew123003 <matthew123003@gmail.com>
2024-09-05 09:53:17 -0400

    OAUTH2 JWT COMPLETE

--- WorkTrackBackEnd/Track/src/main/java/com/Tek/Track/TrackApplication.java
+++ WorkTrackBackEnd/Track/src/main/java/com/Tek/Track/TrackApplication.java
@@ -9,6 +9,6 @@ public class TrackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrackApplication.class, args);
 	}
-	// OAUTH2 JWT WORKING AND RUNNING
+	// OAUTH2 JWT WORKING AND RUNNING, ADDED AUTH SERVICE
 	// EVERYTHING STILL WORKING, NEED TO TEST WITH POSTMAN
 }
