0693be97d6014d8bf3cb5602f1c4e1cc2cac98f7
matthew123003 <matthew123003@gmail.com>
2024-09-29 19:41:15 -0400

    Reinstalled React Native

--- WorkTrackFrontEnd/WorkTrack01/constants/images.js
+++
@@ -1,5 +0,0 @@
-import logoWhite from "../assets/images/logoWhite.png";
-import logoBlack from "../assets/images/logo-black.png";
-import jobLaptop from "../assets/images/finding-job.jpg";
-
-export default { logoWhite, logoBlack, jobLaptop };
