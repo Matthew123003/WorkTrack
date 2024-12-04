eb9e80a70ab61e06fc60ddfcca3b3bfab6239936
matthew123003 <matthew123003@gmail.com>
2024-12-03 19:53:03 -0500

    Updated profile screen

--- WorkTrackFrontEnd/WorkTrack/app/(tabs)/profile.tsx
+++ WorkTrackFrontEnd/WorkTrack/app/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Settings = () => {
+const Profile = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Settings Screen!</Text>
+      <Text style={styles.text}>Welcome to the Profile Screen!</Text>
     </View>
   );
 };
@@ -21,4 +21,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Settings;
+export default Profile;
