afbe62857977592979025f3fbfa7ca59f8bbafe9
matthew123003 <matthew123003@gmail.com>
2024-12-09 11:38:59 -0500

    Adjusted styling

--- WorkTrackFrontEnd/WorkTrack/app/(settings)/_layout.tsx
+++ WorkTrackFrontEnd/WorkTrack/app/(settings)/_layout.tsx
@@ -1,4 +1,3 @@
-// /app/(auth)/_layout.tsx
 import { Slot, useNavigation } from 'expo-router';
 import { View, StyleSheet } from 'react-native';
 import React, { useEffect } from 'react';
@@ -7,7 +6,7 @@ const SettLayout = () => {
   const navigation = useNavigation();
 
   useEffect(() => {
-    // Hide the header for all pages in the (auth) section
+    // Hide the header for all pages in the settings section
     navigation.setOptions({ headerShown: false });
   }, [navigation]);
 
@@ -20,9 +19,8 @@ const SettLayout = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#f5f5f5',
+    flex: 1, // Ensure the container fills the entire screen
+    backgroundColor: '#f5f5f5', // Background color for consistency
   },
 });
 
